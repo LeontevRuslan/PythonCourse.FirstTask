3be7c71629627405663d20777fb4c9bbe477d3eb
Leontev Ruslan <leorus2101@gmail.com>
2025-03-06 21:14:08 +0700

    Add temlates, forms, change views, models, urls

--- NewProject/TaskManager/signals.py
+++ NewProject/TaskManager/signals.py
@@ -1,13 +1,17 @@
 from django.core.mail import EmailMessage
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Task, User
+from .models import Task
 
 def make_notification(email_address):
-    email = EmailMessage('You are get the task', to=['emai_address'])
+    email = EmailMessage(
+        subject='You have been assigned a new task',  # Тема письма
+        body='You have been assigned a new task. Please check your tasks list.',  # Тело письма
+        to=[email_address],
+    )
     email.send()
 
 @receiver(pre_save, sender=Task)
-def handler(instance, **kwargs):
+def task_assignment_handler(instance, **kwargs):
     if instance.assigned_user_id:
         make_notification(instance.assigned_user_id.email)
